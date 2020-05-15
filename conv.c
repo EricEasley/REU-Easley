1c412be2abf6a321fd73cf3b7da6a9908111a899
EricEasley <EricEasley@my.unt.edu>
2020-05-15 09:31:10 -0500

    added support for multiple channels

--- conv.c
+++ conv.c
@@ -1,22 +1,25 @@
+/*
+	Program executes a discrete convolution of a kernel (arg2) over an input feature map (arg1)
+
+	Program will print final result if compiled with -DPRINT flag
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-/* Eric, some comments:
-   1. Move the convolution loops to a separate function of its own
-   2. I assume you are yet to add the number of channels and number of kernels controls to the code
-   3. When you do, you will need an input argument or a header in the input feature map to tell the algorithm
-   how the feature is laid out - HWC or CHW.
-   */
+#define ITERATIONS 1
 
-#define ITERATIONS 1000000
+float *** convolve(float ***imap, float ***kernel, float ***omap, char type[], int channels, int isize, int ksize, int osize);
 
 int main(int argc, char *argv[]){
 	FILE *kernelfile, *inputfile;
-	int k1, k2, i1, i2, o1, o2;
+	int k1, k2, kc, i1, i2, ic, o1, o2, oc;
+	char ktype[4], itype[4];
 	int i, j, x, y;
 	unsigned long long int iterations;
-	int **imap, **kernel, **omap;
+	float ***imap, ***kernel, ***omap;
 	clock_t time;
 
 /*
@@ -40,92 +43,239 @@ int main(int argc, char *argv[]){
 		exit(1);
 	}
 
-	fscanf(inputfile, "%i %i", &i1, &i2);
+	fscanf(inputfile, "%s", itype);
 
-	fscanf(kernelfile, "%i %i", &k1, &k2);
+	fscanf(kernelfile, "%s", ktype);
+
+	if(!strcmp(itype, "CHW")){
+		fscanf(inputfile, "%i %i %i", &ic, &i1, &i2);
+	} else {
+		fscanf(inputfile, "%i %i %i", &i1, &i2, &ic);
+	}
+
+	if(!strcmp(ktype, "CHW")){
+		fscanf(kernelfile, "%i %i %i", &kc, &k1, &k2);
+	} else {
+		fscanf(kernelfile, "%i %i %i", &k1, &k2, &kc);
+	}
 
 	o1 = i1 - k1 + 1;
 	o2 = i2 - k2 + 1;
+	oc = ic;
 
-	imap = (int**) malloc(sizeof(int*) * i1);
-	kernel = (int**) malloc(sizeof(int*) * k1);
-	omap = (int**) malloc(sizeof(int*) * o1);
+	if(!strcmp(itype, "CHW")){
 
-	for(i = 0; i < i1; i++){
-		imap[i] = (int*) malloc(sizeof(int) * i2);
-	}
+		imap = (float***) malloc(sizeof(float**) * ic);
+		kernel = (float***) malloc(sizeof(float**) * kc);
 
-	for(i = 0; i < k1; i++){
-		kernel[i] = (int*) malloc(sizeof(int) * k2);
-	}
+  		for(i = 0; i < ic; i++){
+			imap[i] = (float**) malloc(sizeof(float*) * i1);
+			for(j = 0; j < i1; j++){
+        			imap[i][j] = (float*) malloc(sizeof(float) * i2);
+			}
+  		}
 
-	for(i = 0; i < o1; i++){
-		omap[i] = (int*) malloc(sizeof(int) * o2);
-	}
-
-	for(i = 0; i < k1; i++){
-		for(j = 0; j < k2; j++){
-			fscanf(kernelfile, "%i", kernel[i] + j);
+  		for(i = 0; i < kc; i++){
+  			kernel[i] = (float**) malloc(sizeof(float*) * k1);
+			for(j = 0; j < k1; j++){
+        			kernel[i][j] = (float*) malloc(sizeof(float) * k2);
+      			}
 		}
-	}
 
-	for(i = 0; i < i1; i++){
-                for(j = 0; j < i2; j++){
-                        fscanf(inputfile, "%i", imap[i] + j);
-                }
-        }
+    		for(x = 0; x < kc; x++){
+    			for(i = 0; i < k1; i++){
+	    			for(j = 0; j < k2; j++){
+		    			fscanf(kernelfile, "%f", kernel[x][i] + j);
+  		  		}
+  	  		}
+    		}
+
+    		for(x = 0; x < ic; x++){
+  	  		for(i = 0; i < i1; i++){
+        			for(j = 0; j < i2; j++){
+          				fscanf(inputfile, "%f", imap[x][i] + j);
+        			}
+      			}
+    		}
+
+  	} else { // HWC
+
+  		imap = (float***) malloc(sizeof(float**) * i1);
+	  	kernel = (float***) malloc(sizeof(float**) * k1);
+
+  		for(i = 0; i < i1; i++){
+  			imap[i] = (float**) malloc(sizeof(float*) * i2);
+      			for(j = 0; j < i2; j++){
+        			imap[i][j] = (float*) malloc(sizeof(float) * ic);
+      			}
+  		}
+
+  		for(i = 0; i < k1; i++){
+  			kernel[i] = (float**) malloc(sizeof(float*) * k2);
+      			for(j = 0; j < k2; j++){
+        			kernel[i][j] = (float*) malloc(sizeof(float) * kc);
+      			}
+	  	}
+
+    		for(x = 0; x < k1; x++){
+    			for(i = 0; i < k2; i++){
+	    			for(j = 0; j < kc; j++){
+		    			fscanf(kernelfile, "%f", kernel[x][i] + j);
+  		  		}
+  	  		}
+    		}
+
+    		for(x = 0; x < i1; x++){
+  	  		for(i = 0; i < i2; i++){
+        			for(j = 0; j < ic; j++){
+          				fscanf(inputfile, "%f", imap[x][i] + j);
+        			}
+      			}
+    		}
+
+  	}
 /*
 	Convolution algorithm
 */
 
-	time = clock();
-
-	for(iterations = 0; iterations < ITERATIONS; iterations++){
-
-	for(i = 0; i < o1; i++){
-		for(j = 0; j < o2; j++){
-		omap[i][j] = 0;
-			for(x = 0; x < k1; x++){
-				for(y = 0; y < k2; y++){
-					omap[i][j] += imap[i + x][j + y] * kernel[x][y];
-				}
-			}
-		}
-	}
-
-	}
-
-	time = clock() - time;
-
-	printf("Discrete convolution took %lf seconds for %i iterations\n", (double)time/CLOCKS_PER_SEC, ITERATIONS);
+	omap = convolve(imap, kernel, omap, itype, ic, i1, k1, o1);
 
 #ifdef PRINT
-	for(i = 0; i < o1; i++){
-		for(j = 0; j < o2; j++){
-			printf("%i ", omap[i][j]);
+	for(x = 0; x < oc; x++){
+		for(i = 0; i < o1; i++){
+			for(j = 0; j < o2; j++){
+				if(!strcmp(itype, "CHW")){
+					printf("%f ", omap[x][i][j]);
+				} else {
+					printf("%f ", omap[i][j][x]);
+				}
+			}
+			printf("\n");
 		}
-		printf("\n");
+		if(x != (oc-1)){printf("\n");}
 	}
 #endif
 /*
 	Destruction of dynamic data
 */
+  	if(!strcmp(itype, "CHW")){
+	  	for(i = 0; i < ic; i++){
+      			for(j = 0; j < i1; j++){
+        			free(imap[i][j]);
+      			}
+      			free(imap[i]);
+    		}
 
-	for(i = 0; i < i1; i++){
-                free(imap[i]);
-        }
+    		for(i = 0; i < kc; i++){
+      			for(j = 0; j < k1; j++){
+        			free(kernel[i][j]);
+      			}
+      			free(kernel[i]);
+    		}
 
-        for(i = 0; i < k1; i++){
-                free(kernel[i]);
-        }
+	  	for(i = 0; i < oc; i++){
+      			for(j = 0; j < o1; j++){
+        			free(omap[i][j]);
+      			}
+		  	free(omap[i]);
+	  	}
 
-	for(i = 0; i < o1; i++){
-		free(omap[i]);
-	}
+  	} else {
+
+    		for(i = 0; i < i1; i++){
+      			for(j = 0; j < i2; j++){
+        			free(imap[i][j]);
+      			}
+      			free(imap[i]);
+    		}
+
+    		for(i = 0; i < k1; i++){
+      			for(j = 0; j < k2; j++){
+        			free(kernel[i][j]);
+      			}
+      			free(kernel[i]);
+    		}
+
+	  	for(i = 0; i < o1; i++){
+      			for(j = 0; j < o2; j++){
+        			free(omap[i][j]);
+      			}
+		  	free(omap[i]);
+	  	}
+  	}
 
 	free(kernel);
 	free(imap);
 	free(omap);
 
 	return 0;
+}
+
+float *** convolve(float ***imap, float ***kernel, float ***omap, char type[], int channels, int isize, int ksize, int osize){
+	clock_t time;
+	unsigned long long int iterations;
+	int i, j, x, y, c;
+	float ***temp;
+
+	if(!strcmp(type, "CHW")){
+		temp = (float***) malloc(sizeof(float**) * channels);
+
+		for(i = 0; i < channels; i++){
+			temp[i] = (float**) malloc(sizeof(float*) * osize);
+			for(j = 0; j < osize; j++){
+				temp[i][j] = (float*) malloc(sizeof(float) * osize);
+			}
+		}
+	} else {
+                temp = (float***) malloc(sizeof(float**) * osize);
+
+                for(i = 0; i < osize; i++){
+                        temp[i] = (float**) malloc(sizeof(float*) * osize);
+                        for(j = 0; j < osize; j++){
+                                temp[i][j] = (float*) malloc(sizeof(float) * channels);
+                        }
+                }
+	}
+
+        time = clock();
+
+        for(iterations = 0; iterations < ITERATIONS; iterations++){
+
+		if(!strcmp(type, "CHW")){
+			for(c = 0; c < channels; c++){
+			        for(i = 0; i < osize; i++){
+		        	        for(j = 0; j < osize; j++){
+		                	temp[c][i][j] = 0;
+		                        	for(x = 0; x < ksize; x++){
+		                                	for(y = 0; y < ksize; y++){
+		                                        	temp[c][i][j] += imap[c][i + x][j + y] * kernel[c][x][y];
+		                                	}
+		                        	}
+		                	}
+				}
+			}
+
+        	} else { // HWC
+			for(c = 0; c < channels; c++){
+                                for(i = 0; i < osize; i++){
+                                        for(j = 0; j < osize; j++){
+                                        temp[i][j][c] = 0;
+                                                for(x = 0; x < ksize; x++){
+                                                        for(y = 0; y < ksize; y++){
+                                                                temp[i][j][c] += imap[i + x][j + y][c] * kernel[x][y][c];
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+
+		}
+
+        }
+
+        time = clock() - time;
+
+        printf("Discrete convolution took %lf seconds for %i iterations\n", (double)time/CLOCKS_PER_SEC, ITERATIONS);
+
+	return temp;
 }
